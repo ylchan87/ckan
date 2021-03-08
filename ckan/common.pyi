da8a374cd9f25d71ea0742ed6cb2b1221b1646c8
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-03-08 17:05:12 +0700

    Update ckan

--- ckan/common.pyi
+++ ckan/common.pyi
@@ -1,26 +1,25 @@
-# (generated with --quick)
-
 import flask.globals
 import flask.wrappers
-import typing
-from typing import Any, FrozenSet, Iterable, Iterator, Type
+from typing import Any, FrozenSet, Iterable, List, MutableMapping
 from werkzeug.datastructures import ImmutableDict
 import werkzeug.local
 
-Local: Type[werkzeug.local.Local]
-LocalProxy: Type[werkzeug.local.LocalProxy]
-MutableMapping: Type[typing.MutableMapping]
-c: werkzeug.local.LocalProxy
-config: CKANConfig
 current_app: flask.globals._FlaskLocalProxy
-falsy: FrozenSet[str]
+
+c: werkzeug.local.LocalProxy
 g: werkzeug.local.LocalProxy
+
 local: werkzeug.local.Local
+
+config: CKANConfig
+
 request: CKANRequest
 session: werkzeug.local.LocalProxy
-truthy: FrozenSet[str]
 
-class CKANConfig(typing.MutableMapping):
+truthy: FrozenSet[str]
+falsy: FrozenSet[str]
+
+class CKANConfig(MutableMapping):
     store: dict
     def clear(self) -> None: ...
     def copy(self) -> dict: ...
@@ -29,6 +28,8 @@ class CKANRequest(werkzeug.local.LocalProxy):
     params: ImmutableDict
 
 def _(*args, **kwargs) -> Any: ...
+def ugettext(*args, **kwargs) -> str: ...
+def ungettext(*args, **kwargs) -> str: ...
 def asbool(obj: Any) -> bool: ...
 def asint(obj: Any) -> int: ...
 def aslist(obj: Any, sep: str, strip: bool) -> List: ...
@@ -36,5 +37,3 @@ def is_flask_request() -> bool: ...
 def streaming_response(
     data: Iterable, mimetype: str, with_context: bool
 ) -> flask.wrappers.Response: ...
-def ugettext(*args, **kwargs) -> str: ...
-def ungettext(*args, **kwargs) -> str: ...
