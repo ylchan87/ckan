40d37e55c5854e3a8f49f2f1b24785c10c8b09da
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-03-17 13:41:13 +0700

    Models till U

--- ckan/model/__init__.pyi
+++ ckan/model/__init__.pyi
@@ -1,5 +1,114 @@
 from typing import Any, Callable, Dict, List
 from sqlalchemy.engine import Engine
+from ckan.model.meta import (
+    Session,
+    engine_is_sqlite,
+    engine_is_pg,
+)
+from ckan.model.core import (
+    State,
+)
+from ckan.model.system import (
+    System,
+)
+from ckan.model.package import (
+    Package,
+    PackageMember,
+    PACKAGE_NAME_MIN_LENGTH,
+    PACKAGE_NAME_MAX_LENGTH,
+    PACKAGE_VERSION_MAX_LENGTH,
+    package_table,
+    package_member_table,
+)
+from ckan.model.tag import (
+    Tag,
+    PackageTag,
+    MAX_TAG_LENGTH,
+    MIN_TAG_LENGTH,
+    tag_table,
+    package_tag_table,
+)
+from ckan.model.user import (
+    User,
+    user_table,
+)
+from ckan.model.group import (
+    Member,
+    Group,
+    group_table,
+    member_table,
+)
+from ckan.model.group_extra import (
+    GroupExtra,
+    group_extra_table,
+)
+from ckan.model.package_extra import (
+    PackageExtra,
+    package_extra_table,
+)
+from ckan.model.resource import (
+    Resource,
+    DictProxy,
+    resource_table,
+)
+from ckan.model.resource_view import (
+    ResourceView,
+    resource_view_table,
+)
+from ckan.model.tracking import (
+    tracking_summary_table,
+    TrackingSummary,
+    tracking_raw_table,
+)
+from ckan.model.rating import (
+    Rating,
+    MIN_RATING,
+    MAX_RATING,
+)
+from ckan.model.package_relationship import (
+    PackageRelationship,
+    package_relationship_table,
+)
+from ckan.model.task_status import (
+    TaskStatus,
+    task_status_table,
+)
+from ckan.model.vocabulary import (
+    Vocabulary,
+    VOCABULARY_NAME_MAX_LENGTH,
+    VOCABULARY_NAME_MIN_LENGTH,
+)
+from ckan.model.activity import (
+    Activity,
+    ActivityDetail,
+    activity_table,
+    activity_detail_table,
+)
+from ckan.model.term_translation import (
+    term_translation_table,
+)
+from ckan.model.follower import (
+    UserFollowingUser,
+    UserFollowingDataset,
+    UserFollowingGroup,
+)
+from ckan.model.system_info import (
+    system_info_table,
+    SystemInfo,
+    get_system_info,
+    set_system_info,
+    delete_system_info,
+)
+from ckan.model.domain_object import (
+    DomainObjectOperation,
+    DomainObject,
+)
+from ckan.model.dashboard import (
+    Dashboard,
+)
+from ckan.model.api_token import (
+    ApiToken,
+)
 
 DB_CONNECT_RETRIES: int
 
