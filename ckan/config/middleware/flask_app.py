2f568104ecec57afd088119deb59b6afe85c53fc
ylchan87 <ylchan87@gmail.com>
2022-09-07 13:37:56 +0000

    manual apply patch https://github.com/ckan/ckan/pull/6954/files to fix inode used up problem

--- ckan/config/middleware/flask_app.py
+++ ckan/config/middleware/flask_app.py
@@ -164,14 +164,27 @@ def make_flask_stack(conf):
         from werkzeug.debug import DebuggedApplication
         app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
-    # Use Beaker as the Flask session interface
     class BeakerSessionInterface(SessionInterface):
         def open_session(self, app, request):
             if 'beaker.session' in request.environ:
                 return request.environ['beaker.session']
-
+    
         def save_session(self, app, session, response):
             session.save()
+    
+        def is_null_session(self, obj):
+    
+            is_null = super(BeakerSessionInterface, self).is_null_session(obj)
+    
+            if not is_null:
+                # Beaker always adds these keys on each request, so if these are
+                # the only keys present we assume it's an empty session
+                is_null = (
+                    sorted(obj.keys()) == [
+                        "_accessed_time", "_creation_time", "_domain", "_path"]
+                )
+    
+            return is_null
 
     namespace = 'beaker.session.'
     session_opts = {k.replace('beaker.', ''): v
